9695b2d8d37eb27ab9932a5c5712cd641d5377f0
KINOSHITA Masanobu <kinoshitamasanobu@KINOSHITAs-MacBook-Air.local>
2026-02-13 00:02:29 +0900

    bugfix

--- themes/setae-theme/header.php
+++ themes/setae-theme/header.php
@@ -4,6 +4,14 @@
 <head>
     <meta charset="<?php bloginfo('charset'); ?>">
     <meta name="viewport" content="width=device-width, initial-scale=1.0, maximum-scale=1.0, user-scalable=no">
+
+    <meta name="theme-color" content="#f5f7fa">
+
+    <meta name="apple-mobile-web-app-title" content="SETAE">
+
+    <link rel="apple-touch-icon" href="<?php echo get_template_directory_uri(); ?>/images/apple-touch-icon.png">
+
+    <link rel="icon" href="<?php echo get_template_directory_uri(); ?>/images/favicon.ico">
     <title>
         <?php wp_title('|', true, 'right'); ?>
     </title>
